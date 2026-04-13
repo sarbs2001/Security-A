2515f1eb3437f9d342223cc39467217450283db8
sarbs2001 <sarbs2001@gmail.com>
2026-04-13 00:55:39 +0000

    added length, uppercase, and lowercase logic // Need to clean repetitive if/else statments, if can //  NEXT: need to add "atleast one number" logic and "special character" logic.

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+def Analyzer(password):
+    chars = list(password)
+    
+    length = 0
+    uppercase_char = 0
+    lowercase_char = 0
 
+    for char in chars:
+        length += 1
+        if char.isupper():
+            uppercase_char += 1
+        if char.islower():
+            lowercase_char += 1
+
+    if length >= 8:
+        print("Password length is good. OK")
+    else:
+        print("Password length is short: NOT RECOMMENDED")
+    if uppercase_char > 0:
+        print("Password has atleast one uppercase letter. OK")
+    else: 
+        print("Does not have atleast one uppercase letter. NOT RECOMMENDED")
+    if lowercase_char > 0:
+        print("Password has atleast one lowercase letter. OK")
+    else:
+        print("Does not have atleast one lowercase letter. NOT RECOMMENDED")
+    
+
+password = str(input("Enter the password here: (Password should be atleast 8 characters with atleast one uppercase, lowercase, number, and special character.)"))
+Analyzer(password)
