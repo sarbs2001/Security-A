cbb4c1515fdc8637e331522b7ff13dd9001f17e8
sarbs2001 <sarbs2001@gmail.com>
2026-04-14 02:51:28 +0000

    added number and special character logic // still need to check if there is a neater way to write bulky if/else statements // planning on making it a interface next

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ def Analyzer(password):
     length = 0
     uppercase_char = 0
     lowercase_char = 0
+    num = 0
+    special = 0
+
+    numbers = ("1", "2", "3", "4", "5", "6", "7", "8", "9")
+    special_character = ("!", "@", "#", "$", "%", "&", "-", "_", "+", "<", ">", "?", "/", "~")
 
     for char in chars:
         length += 1
@@ -11,19 +16,43 @@ def Analyzer(password):
             uppercase_char += 1
         if char.islower():
             lowercase_char += 1
+        if char in numbers:
+            num +=1
+        if char in special_character:
+            special += 1
 
     if length >= 8:
-        print("Password length is good. OK")
+        print(f"Password is {length} characters: GOOD")
     else:
-        print("Password length is short: NOT RECOMMENDED")
+        print(f"Password length short by {8 - length} characters: NOT RECOMMENDED")
     if uppercase_char > 0:
-        print("Password has atleast one uppercase letter. OK")
+        if uppercase_char > 1:
+            print(f"Has {uppercase_char} uppercase letters: GOOD")
+        else:
+            print(f"Has {uppercase_char} uppercase letter: GOOD")
     else: 
-        print("Does not have atleast one uppercase letter. NOT RECOMMENDED")
+        print(f"Has 0 uppercase letters: NOT RECOMMENDED")
     if lowercase_char > 0:
-        print("Password has atleast one lowercase letter. OK")
+        if lowercase_char > 1:
+            print(f"Has {lowercase_char} lowercase letters: GOOD")
+        else:
+            print(f"Has {lowercase_char} lowercase letter: GOOD")
     else:
-        print("Does not have atleast one lowercase letter. NOT RECOMMENDED")
+        print("Has 0 lowercase letters: NOT RECOMMENDED")
+    if num > 0:
+        if num > 1:
+            print(f"Has {num} numbers: GOOD")
+        else:
+            print(f"Has {num} number: GOOD")
+    else: 
+        print("Has 0 numbers: NOT RECOMMENDED")
+    if special > 0:
+        if special > 1:
+            print(f"Has {special} special characters: GOOD")
+        else:
+            print(f"Has {special} special character: GOOD")
+    else:
+        print("Has 0 special characters: NOT RECOMMENDED")
     
 
 password = str(input("Enter the password here: (Password should be atleast 8 characters with atleast one uppercase, lowercase, number, and special character.)"))
